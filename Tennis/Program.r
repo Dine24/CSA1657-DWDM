903f352ea6d40427251d1e772d63b5f6feaf78e7
Dinesha G <120408570+Dine24@users.noreply.github.com>
2024-06-17 12:21:32 +0530

    Create Program.r

---
+++ Tennis/Program.r
@@ -0,0 +1,4 @@
+points_scored <- c(10, 15, 20, 25, 30, 40, 50, 60, 70, 80, 100, 120, 150)
+boxplot(points_scored,
+        main = "Boxplot of Points Scored by Tennis Players",
+        ylab = "Points Scored")
